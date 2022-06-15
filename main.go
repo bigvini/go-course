24c82c0c9cfbb129cedbd56d59ec16d9e082b568
bigvini <98239700+bigvini@users.noreply.github.com>
2022-06-15 12:04:36 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -7,31 +7,31 @@ import (
 func main() {
 
 	const (
-		oneApple = 5.99
-		onePear  = 7.00
-		myMoney  = 23.00
+		oneApplePrice = 5.99
+		onePearPrice  = 7.00
+		myMoney       = 23.00
 	)
 
-	fmt.Println("Одне яблуко коштує ", oneApple, "грн.")
-	fmt.Println("Одна груша коштує ", onePear, "грн.")
+	fmt.Println("Одне яблуко коштує ", oneApplePrice, "грн.")
+	fmt.Println("Одна груша коштує ", onePearPrice, "грн.")
 	fmt.Println("Ми маємо ", myMoney, "грн.")
 
 	fmt.Println(" Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
-	nineApple := 9 * oneApple
-	sevenPear := 9 * onePear
+	nineApple := 9 * oneApplePrice
+	sevenPear := 9 * onePearPrice
 	fmt.Println("Щоб купити 9 яблук потрібно: ", nineApple, "грн")
 	fmt.Println("Щоб купити 7 груш потрібно: ", sevenPear, "грн")
 
 	fmt.Println(" Скільки груш ми можемо купити?")
-	totalBuyPears := myMoney / onePear
+	totalBuyPears := myMoney / onePearPrice
 	fmt.Println("Ми можемо купити", int(totalBuyPears), "груші.")
 
 	fmt.Println("Скільки яблук ми можемо купити?")
-	totalBuyAppels := myMoney / oneApple
+	totalBuyAppels := myMoney / oneApplePrice
 	fmt.Println("Ми можемо купити", int(totalBuyAppels), "яблука.")
 
 	fmt.Println("Чи ми можемо купити 2 груші та 2 яблука?")
-	twoAppelAndTwoPear := 2*oneApple + 2*onePear
+	twoAppelAndTwoPear := 2*oneApplePrice + 2*onePearPrice
 
 	fmt.Println("Ми неможем купити 2 груші і 2 яблука. Тому що це буде ", twoAppelAndTwoPear, "грн.")
 
